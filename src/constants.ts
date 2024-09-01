a552fbd4686773f069ff97ff34c93ddda1962b81
Natalia Boudard <natalia.boudard@gmail.com>
2024-09-01 13:59:43 +0200

    dockerfile & nginx.conf

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'non' : 'http://localhost:5000';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:5000' : 'http://localhost:5000';
